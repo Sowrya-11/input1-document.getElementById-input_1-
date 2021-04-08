431359f18aa6a9b650901868d8e3191f6fa4f9d6
Sowrya-11 <76764710+Sowrya-11@users.noreply.github.com>
2021-04-08 23:26:40 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,16 @@
+para1= document.getElementById("para_1")
+para2= document.getElementById("para_2")
+input1 = document.getElementById("input_1")
+input1 = document.getElementById("2input_1")
+
+//mam it is correct but now working
+function sub1(){
+    para1.join("input_1")
+    document.getElementById("blank").innerHTML=para1;
+    
+}
+function sub2(){
+    para1.join("input_1")
+    document.getElementById("blank").innerHTML=para2;
+    
+}
